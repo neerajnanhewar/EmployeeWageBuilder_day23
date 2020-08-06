5c80f2c691d1f27e3110c85c48df0f00f38b4256
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-08-06 13:35:14 +0530

    Added : Welcome Message

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,10 +1,9 @@
 // Employee Wage Computation Problem //
 class EmployeeWageBuilder {
-	public void welcome(){
+	public static void welcome(){
 		System.out.println("Welcome To Employee Wage Computation ");
 	}
 	public static void main(String[] args){
-		EmployeeWageBuilder ewb= new EmployeeWageBuilder();
-		ewb.welcome();
+		welcome();
 	}
 }
