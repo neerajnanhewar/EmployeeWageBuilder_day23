668579a5f82fd2494cd238b7b9507c2f2af90fa4
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-08-08 15:47:46 +0530

    Added : Interface update

--- IEmpWage.java
+++ IEmpWage.java
@@ -1,4 +1,5 @@
 public interface IEmpWage{
 	public void addCompanyEmpWage(String company, int emp_Rate_PerHr, int total_Work_Day, int working_Hrs);
 	public void computeEmpWage();
+	public int getTotalWage(String companyName);
 }
